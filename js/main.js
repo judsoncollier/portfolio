8eb2875dbddf6362c5d3188182e9d38e5e65fc4f
Judson Collier <judson@judsoncollier.com>
2013-01-05 02:03:15 -0500

    OMG YES

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
 	$('#nav #about-link').click(function(){
 		$('html, body').animate({
-			scrollTop: $("#about").offset().top
+			scrollTop: $("#about").offset().top - 100
 		}, 2000);
 	})
 	$('#nav #work').click(function(){
 		$('html, body').animate({
-			scrollTop: $("#worksamples").offset().top
+			scrollTop: $("#worksamples").offset().top - 95
 		}, 2000);
 	})
 	$('#nav #contact').click(function(){
 		$('html, body').animate({
-			scrollTop: $("#contact").offset().top
+			scrollTop: $("#contact").offset().top - 10
 		}, 2000);
 	})
 
@@ -70,7 +70,6 @@ $(document).ready(function() {
 			return;
 		}
 	})
-
 	$(".ul-o-death a.hoverimg").hover(
   		function () {
   			$(this).find('.overlay').animate({ 'opacity': '1'}, 300, 'linear');
@@ -80,4 +79,77 @@ $(document).ready(function() {
   			$(this).find('.overlay').animate({ 'opacity': '0' }, 300, 'linear');
   		}
 	);
+	$(function(){
+		var nav_container = $('#nav_container');
+		var nav = $('#nav');
+		$('#nav_container').waypoint( function(direction) {
+			if (direction ==='down'){
+				nav.toggleClass('sticky').animate({ backgroundColor: "rgba(255,255,255,.9)", boxShadow: "0px 3px 5px rgba(0,0,0,.1)", }, "400");
+			} else {
+				nav.toggleClass('sticky').animate({ backgroundColor: "transparent", boxShadow: "0px 3px 5px rgba(0,0,0,.0)" }, "fast");
+			}
+		});
+	});
+
+	//update navigation while scrolling
+
+	//para-para-parallax
+
+	var $window = $('window');
+	var $first = $('.parallax');
+
+	var windowHeight = $window.height();
+
+	$('#parallax').bind('inview', function (event, visible){
+		if (visible == true) {
+			$(this).addClass("inview");
+		} else {
+			$(this).removeClass("inview");
+		}
+	})
+	function RepositionNav(){
+	    var windowHeight = $window.height();
+	    var navHeight = $('#nav').height() / 3;
+	    var windowCenter = (windowHeight / 2);
+	    var newtop = windowCenter - navHeight;
+	    $('#nav').css({"top": newtop});
+	}
+	function newPos(x, windowHeight, pos, adjuster, inertia){ return x + "%" + (-((windowHeight + pos) - adjuster) * inertia) + "px"; }
+
+
+	function move() {
+		var pos = $(window).scrollTop();
+		if($('.parallax').hasClass("inview")){
+			console.log('hi')
+			//call the newPos function and change the background position
+			$first.css({'backgroundPosition': newPos(75, 5000, -pos, 100, .3) + ", " + //sem
+					newPos(51, 150, -pos, 5, 0.1) + ", " + //spring formal
+					newPos(28, -0, -pos, 0, 10) + ", " + //mute
+					newPos(53, -0, -pos, 4, 0.2) + ", " + //music
+					newPos(78.5, -203, pos, 0, 0.2) + ", " + //phantom
+					newPos(93, -0, pos, 0, 0.5) + ", " + // covers xxi
+					newPos(10, -0, -pos, 0, 0.55) + ", " + // pepband
+					newPos(45, -20, -pos, 1, 0.3) + ", " + // madness
+					newPos(38, -0, pos, 1, 0.5) + ", " + // tvf
+					newPos(44, -0, pos, 5, 0.9) + ", " + // monsters ball
+					newPos(55, -0, -pos, 5, 0.9) + ", " + // iwomen
+					newPos(77, -0, pos, 5, 0.5) + ", " + // dub dub 
+					newPos(21, -00, pos, 5, 0.5) + ", " + // xix
+					newPos(21, -50, pos, 5, 0.5) + ", " + // badgerbash
+					newPos(35, -20, -pos, 3, 0.1) + ", " + //polankis
+					newPos(72.5, -00, -pos, 5, 0.24) + ", " + // lori
+					newPos(28.5, -10, -pos, 5, 0.2) + ", " + // anna 
+					newPos(91, -0, -pos, 0, 0.3) + ", " + // preston
+					newPos(9, -1800, pos, 0, 0.2)}); //dorminy 
+		}
+	}
+	$(window).resize(function(){
+	    move();
+	    RepositionNav();
+	});
+	$(window).bind('scroll', function(){
+	    move();
+	});
+
+
 });
